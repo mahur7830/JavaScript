aa22fc26ae9835e53c22f12d4c163b87b8648869
ANIL KUMAR <141898587+mahur7830@users.noreply.github.com>
2024-05-02 02:52:56 +0000

    part1 done

---
+++ basics/part1.js
@@ -0,0 +1,23 @@
+// let newDate=new Date();
+// console.log(newDate.toDateString());
+// console.log(newDate.toLocaleDateString());
+
+// let date=new Date("02-05-2024");
+// console.log(date.toLocaleString());
+
+//let timeStamp= Date.now();
+//console.log(timeStamp);
+// ------------to convert miliscond time into second----------------//
+//console.log(Math.floor(Date.now()/1000));
+
+// let date=new Date;
+// console.log(date.getMonth());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+// console.log(date.getUTCFullYear());
+
+// let newDate=new Date();
+// newDate.toLocaleString('default',{
+//     weekday:'long'
+// })
+// console.log(newDate);
