8d65dffef56b9f2597109b75aedc97605bebd9da
ANIL KUMAR <141898587+mahur7830@users.noreply.github.com>
2024-05-02 10:49:32 +0000

    part2 done

---
+++ basics/part2.js
@@ -0,0 +1,52 @@
+// Array
+// const arr=[1,2,3,4,5,6,7];
+// arr.unshift(9);
+// console.log(arr);
+// arr.shift()
+// arr.unshift(10);
+// arr.shift();
+// console.log(typeof true)
+// const arr1=[1,2,3,4,5];
+// const arr2=[12,13,14,15];
+// let newarr=arr1.join();
+// console.log(arr1);
+// console.log(newarr);
+
+
+
+// const arr=[1,2,3,4,5,6,7];
+// console.log("A",arr.slice(1,4));
+// console.log("B",arr.splice(1,4));
+// console.log(arr.splice(7,8));
+
+
+/// arr1.push(arr2);
+// console.log(arr1);
+// console.log(arr2);
+
+//console.log(arr1.concat(arr2));
+
+//------ Spread---------------------------------------------------------------------------//
+// let all=[...arr1,...arr2]
+// console.log(all);
+
+//---------Use of flate------------------------------------------------------------------//
+// let arr=[1,2,3,[3,2,1],34,2,[4,5,6[6,9,8,[8,76,5,4]]]]
+// let newarr=arr.flat(Infinity);
+// console.log(newarr);
+
+
+      //console.log(Array.isArray("Anil kumar"));
+
+//------This converts a string into an Array---------------------------------------------//
+      //console.log(Array.from("Anil Kumar"));
+
+//---------------interview question----------------------------------------------------//
+//console.log(Array.from({ame:"Anil"}));//---this will give an empty array----------------
+
+
+//-----------------of() method ka use-------------------//
+// let score1=100;
+// let score2=200;
+// let score3=300;
+// console.log(Array.of(score1,score2,score3));
